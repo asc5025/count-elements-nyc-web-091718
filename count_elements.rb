9eadc41c81f58eabcff725918ae700fb45ef6ffb
Albert Chun <albert.s.chun@gmail.com>
2018-09-18 12:13:51 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,8 +3,9 @@ require 'pry'
 def count_elements(array)
   count_hash = {}
   array.each do |element| 
-    
-    count_hash[element] += 1 
-  count_hash
+    if count_hash[element] == element
+      count_hash[element] += 1
+    else
+      count_hash[element]
 end
  
