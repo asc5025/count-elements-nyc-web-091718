204b5b556495c43ed22d7362c2fe64a6ee6b33e4
Albert Chun <albert.s.chun@gmail.com>
2018-09-18 12:46:53 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def count_elements(array)
   count_hash = {}
-  array.each do |element| 
+  array.each do |element|
+    count_hash << count_hash[:element]
     binding.pry
-      count_hash << count_hash[:element]
-      count_hash[:element] += 1
-      count_hash
+    count_hash[:element] += 1
+    count_hash
   end
 end
  
