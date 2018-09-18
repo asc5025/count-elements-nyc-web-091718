bfa43dba31f3ca04eed5721ca55736ecdbe267ec
Albert Chun <albert.s.chun@gmail.com>
2018-09-18 12:50:03 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
-#def count_elements(array)
-  #count_hash = {}
-  #array.each do |element|
-    #count_hash << count_hash[:element]
-    #count_hash[:element] += 1
-    #count_hash
-  #end
-#end
-
 def count_elements(array)
   count_hash = Hash.new(0)
   array.each do |item| 
